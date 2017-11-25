b1657abb85b5b25630cb8ef498984b5a9dab512d
Heather Cates <cates.heather.a@gmail.com>
2017-11-24 16:34:16 -0800

    made list with emokies in it

--- ListPractice/ViewController.swift
+++ ListPractice/ViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {//needs data scource and delegate to populate rows
 
+    @IBOutlet weak var tableview: UITableView!
+    
+    let emojis = ["🐭","🐸","🐵"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableview.dataSource = self
+        tableview.delegate = self
     }
+    //get number of rows needed
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    //what to populate rows with
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
